c55c190468c0b9dbf9c540d8b47a5a370f4cf4a7
Tony Joblin <tonyjoblin@yahoo.com.au>
2019-03-22 19:46:42 +1000

    Component testing
    Added snap shot test for ArticleList.

---
+++ lib/components/ArticleList.spec.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ArticleList from './ArticleList';
+
+describe('ArticleList', () => {
+  const testProps = {
+    articles: {
+      a: { id: 'a' },
+      b: { id: 'b' },
+    },
+    articleActions: {
+      lookupAuthor: jest.fn(() => ({})),
+    }
+  };
+
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      (<ArticleList {...testProps} />),
+    ).toJSON();
+
+    expect(tree.children.length).toBe(2);
+    expect(tree).toMatchSnapshot();
+  });
+});
